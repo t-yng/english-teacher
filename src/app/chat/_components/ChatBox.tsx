b8bc505367cfe9b39d34e7b72aab9a384212f069
t-yng <t.yng.greenmonkey@gmail.com>
2023-10-11 00:17:10 +0900

    チャットでの質問の入力をtextareaに変更

--- src/app/chat/_components/ChatBox.tsx
+++ src/app/chat/_components/ChatBox.tsx
@@ -1,15 +1,35 @@
-import { FC, FormEvent, useCallback, useState } from "react";
+import {
+  ChangeEvent,
+  FC,
+  FormEvent,
+  KeyboardEvent,
+  KeyboardEventHandler,
+  useCallback,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import styles from "./ChatBox.module.css";
+import { useAutoResize } from "./useAutoResize";
 
 type ChatBoxProps = {
   onSubmit?: (text: string) => void;
 };
 
 export const ChatBox: FC<ChatBoxProps> = ({ onSubmit }) => {
+  const formRef = useRef<HTMLFormElement>(null);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
   const [text, setText] = useState("");
+  const { autoResize } = useAutoResize();
+
+  useLayoutEffect(() => {
+    if (!textareaRef.current) return;
+    autoResize(textareaRef.current);
+  }, [autoResize]);
 
   const handleSubmit = useCallback(
     (e: FormEvent<HTMLFormElement>) => {
+      console.log("handleSubmit");
       e.preventDefault();
       onSubmit?.(text);
       setText("");
@@ -17,14 +37,36 @@ export const ChatBox: FC<ChatBoxProps> = ({ onSubmit }) => {
     [onSubmit, text]
   );
 
+  const handleChangeText = useCallback(
+    (e: ChangeEvent<HTMLTextAreaElement>) => {
+      autoResize(e.target);
+      setText(e.target.value);
+    },
+    [autoResize]
+  );
+
+  const handleKeyDown: KeyboardEventHandler<HTMLTextAreaElement> = useCallback(
+    (e: KeyboardEvent<HTMLTextAreaElement>) => {
+      // cmd + enter を押したときにフォームの送信を実行する
+      if (e.key === "Enter" && e.metaKey) {
+        formRef.current?.dispatchEvent(
+          new Event("submit", { cancelable: true, bubbles: true })
+        );
+      }
+    },
+    []
+  );
+
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <input
-        type="text"
+    <form className={styles.form} onSubmit={handleSubmit} ref={formRef}>
+      <textarea
+        rows={1}
         placeholder="質問する"
         className={styles.chatBox}
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={handleChangeText}
+        onKeyDown={handleKeyDown}
+        ref={textareaRef}
       />
     </form>
   );
